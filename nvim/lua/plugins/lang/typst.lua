56b3fa3690d2b441216c1212c936a68286159d70
driedyellowpeach <wrz750726@gmail.com>
2026-01-17 11:43:24 -0800

    feat: typst lsp update

--- nvim/lua/plugins/lang/typst.lua
+++ nvim/lua/plugins/lang/typst.lua
@@ -1,38 +1,50 @@
-vim.filetype.add({ extension = { typ = "typst" } })
-
--- disable spell checking for typst
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "typst" }, -- Replace with your file types
-  callback = function()
-    vim.opt_local.spell = true
-  end,
-})
-
-vim.lsp.config["tinymist"] = {
-  cmd = { "tinymist" },
-  filetypes = { "typst" },
-  settings = {
-    exportPdf = "onType",
-    outputPath = "$root/target/$dir/$name",
-  },
-}
-
 return {
   {
-    "stevearc/conform.nvim",
-    opts = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          typst = { "typstyle" },
-        },
-        formatters = {
-          typstyle = {
-            -- Change where to find the command
-            command = "/opt/homebrew/bin/typstyle",
-            args = {},
-            stdin = true,
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "typst" },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {
+          settings = {
+            formatterMode = "typstyle",
+            exportPdf = "onType",
+            outputPath = "$root/target/$dir/$name",
           },
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<leader>cp", function()
+              client:exec_cmd({
+                title = "pin",
+                command = "tinymist.pinMain",
+                arguments = { vim.api.nvim_buf_get_name(bufnr) },
+              }, { bufnr = bufnr })
+              vim.notify("Pinned: " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+            end, { buffer = bufnr, desc = "Tinymist Pin Main" })
+
+            vim.keymap.set("n", "<leader>cu", function()
+              client:exec_cmd({
+                title = "unpin",
+                command = "tinymist.pinMain",
+                arguments = { vim.v.null },
+              }, { bufnr = bufnr })
+              vim.notify("Unpinned main file", vim.log.levels.INFO)
+            end, { buffer = bufnr, desc = "Tinymist Unpin Main" })
+          end,
         },
+      },
+    },
+    init = function()
+      vim.filetype.add({ extension = { typ = "typst" } })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "typst" },
+        callback = function()
+          vim.opt_local.spell = true
+        end,
       })
     end,
   },
@@ -41,12 +53,9 @@ return {
     "chomosuke/typst-preview.nvim",
     ft = "typst",
     version = "1.*",
-    build = function()
-      require("typst-preview").update()
-    end,
+    opts = {},
   },
-  -- NOTE: using tinymist as typst language server
-  -- https://github.com/Myriad-Dreamin/tinymist
+
   {
     "mason-org/mason.nvim",
     opts = {
